dafa30061f015ed3e77136a8de51af33187ccb5b
Brogy-alt <brogangys2@gmail.com>
2022-11-08 10:01:33 +0200

    shu shu fu ho

---
+++ js/code.js
@@ -0,0 +1,27 @@
+/*let numb1 = 9+'Joel';
+// console.log(numb1)*/
+
+
+// prompt
+
+// let numb1 = prompt("Please enter a numb1: ");
+// let numb2 = prompt ("Please enter numb2 ");
+// let addition =numb1 + numb2;
+// console.log(addition);
+
+// eval
+
+let numb1 = 4;
+let numb2 = 3;
+let product = eval (`${numb1}*${numb2}`);
+console.log(product);
+
+// ${}
+
+
+// challenge 
+
+
+
+
+
